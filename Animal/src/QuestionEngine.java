088c4ed402ca7b047624419dbee7f2c4bb2689d9
Mateusz Namysl <mateusz.namysl0318@gmail.com>
2025-12-15 22:05:51 +0100

    Initial commit

---
+++ Animal/src/QuestionEngine.java
@@ -0,0 +1,63 @@
+import CLIPSJNI.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionEngine {
+
+    private final Environment clips;
+
+    public QuestionEngine() throws Exception {
+        clips = new Environment();
+        clips.load("winedemo.clp");
+        clips.reset();
+        clips.run();
+    }
+
+    public boolean hasResult() throws Exception {
+        PrimitiveValue wynik =
+                clips.eval("(find-all-facts ((?w wynik)) TRUE)");
+        return wynik.size() > 0;
+    }
+
+    public String getResultText() throws Exception {
+        PrimitiveValue wynik =
+                clips.eval("(find-all-facts ((?w wynik)) TRUE)");
+
+        PrimitiveValue w = wynik.get(0);
+        String zwierze = w.getFactSlot("zwierze").stringValue();
+        String klucz = w.getFactSlot("klucz").stringValue();
+
+        return "<html><div style='text-align:center'>" +
+               "<b>WYNIK</b><br><br>" +
+               "Zwierzę: <b>" + zwierze + "</b><br>" +
+               "Klucz: <b>" + klucz + "</b>" +
+               "</div></html>";
+    }
+
+    public Question getCurrentQuestion() throws Exception {
+        PrimitiveValue pytania =
+                clips.eval("(find-all-facts ((?p pytanie)) TRUE)");
+
+        if (pytania.size() == 0) {
+            return null;
+        }
+
+        PrimitiveValue p = pytania.get(0);
+        String text = p.getFactSlot("tresc").stringValue();
+        PrimitiveValue odp = p.getFactSlot("odpowiedzi");
+
+        List<String> answers = new ArrayList<>();
+        for (int i = 0; i < odp.size(); i++) {
+            answers.add(odp.get(i).stringValue());
+        }
+
+        return new Question(text, answers);
+    }
+
+    public void answer(String answerText) throws Exception {
+        String fact =
+                "(odpowiedz (tresc \"" + answerText + "\"))";
+        clips.assertString(fact);
+        clips.run();
+    }
+}
